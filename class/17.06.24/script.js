59c4e47f77c7d00bbe9bd0549af01d9b5673f7f9
Margarita Borkivets <65771267+borkivetsm@users.noreply.github.com>
2024-06-23 10:26:25 +0200

    Add files via upload

---
+++ class/17.06.24/script.js
@@ -0,0 +1,54 @@
+console.log('Marharyta'.length);
+console.log('Marharyta'.toUpperCase());
+
+
+// var a = 10;
+
+// const b = 20;
+// let c = 25;
+
+
+// c = 30;
+
+// let age = 25;
+
+
+// age = 30
+// console.log(ages);
+
+
+// const a = 5;
+// const b = 25.5;
+
+
+// const c = 'hello';
+
+
+// const d = true;
+
+// const isModalOpen = false;
+
+// let selectedProduct = null;
+
+// let userName;
+// console.log(userName)
+
+
+
+// const message = 'HelloGoIteens';
+// console.log(message);
+
+// // const userName = 'Nick';
+// // console.log("Username is ", userName)
+// alert(message)
+
+// const message = confirm("Do you agree with the terms?")
+// console.log(message)
+
+// const message = prompt("Enter your age")
+// console.log(message)
+
+
+
+const groupName = "19"
+console.log("Я навчаюся в GoITeens в групі: ", groupName);
