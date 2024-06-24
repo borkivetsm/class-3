c48b284fa873c363e00fd35bce52f45468c2d582
Margarita Borkivets <margo.borkivets@gmail.com>
2024-06-24 16:53:34 +0200

    hw from 17.06

---
+++ hw_17.06.24/script.js
@@ -0,0 +1,50 @@
+"use strict"
+
+let age = 14;
+console.log(age);
+
+
+const name = "Margo";
+console.log(name);
+
+
+let isStudent = true;
+console.log(isStudent);
+
+
+let myString = "I have no favourite quote";
+console.log(myString);
+
+
+let myNumber = 5;
+ 
+myNumber = (myNumber + 10);
+console.log(myNumber);
+
+
+const myNull = null;
+console.log(myNull);
+
+
+// const ask = prompt("Enter your name");
+
+// console.log("Hello,", ask);
+
+
+// const confirmation = confirm("Do you confirm your age?");
+// if(confirmation) {
+//     console.log("Thank you for the confirmation!");
+// }
+// else {
+//     console.log("Confirmation declined")
+// }
+
+
+alert("This action is dangerous!");
+const actionConfirm = confirm('Do you confirm?');
+if(actionConfirm) {
+    alert("Thank you for the confirmation!")
+}
+else {
+    alert("Action declined")
+}
