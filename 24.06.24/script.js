de218469c5a44776210dd919448f6b1506c3a62b
Margarita Borkivets <margo.borkivets@gmail.com>
2024-06-26 14:53:12 +0200

    .

---
+++ 24.06.24/script.js
@@ -0,0 +1,75 @@
+
+// const a = 20;
+// const b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// console.log(a > b);
+// console.log(a < b);
+// a >= b;
+// a <= b;
+// a == b;
+// a != b;
+// console.log(a === b);
+// console.log(a != b)
+
+
+// 5 == '5' //true
+// 5 === '5' //false
+
+
+
+// const c = "20";
+// console.log(Number(c));
+
+// const d = "qwe";
+// console.log(Number(d));
+
+
+// console.log(Number.parseInt('10px'));
+// console.log(Number.parseInt('15qwe24'));
+// console.log(Number.parseInt('14.45qwe'));
+// console.log(Number.parseInt('qwe'));
+
+// console.log(Number.parseFloat('10px'));
+// console.log(Number.parseFloat('12qwe24'));
+// console.log(Number.parseFloat('12.24qwe78'));
+
+
+
+
+// const x = Number('25');
+// console.log(Number.isNaN(x));
+
+// const y = Number('qweqwe');
+// console.log(Number.isNaN(y));
+
+// console.log(0.1 + 0.3 === 0.4);
+
+
+// console.log(Math.floor(2.8));
+// console.log(Math.ceil(3.4));
+
+// console.log(Math.round(1.2));
+// console.log(Math.round(1.5));
+
+// console.log(Math.max(20, 100, 47, 124, 99));
+// console.log(Math.min(20, 100, 47, 124, 99));
+
+// console.log(Math.pow(2,3));
+
+// console.log(Math.random() * (10 -1) + 1)
+
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+
+console.log(a1, a2, a3, a4, a5);
