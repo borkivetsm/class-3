14793db6c4a75c6287cee4ce601bae720b0bfd36
Margarita Borkivets <margo.borkivets@gmail.com>
2024-07-08 19:50:33 +0200

    classwork added

---
+++ 08.07.24/script.js
@@ -0,0 +1,111 @@
+// &&
+// const num = 25;
+// const result = num > 15 && num < 35;
+// console.log(result);
+
+// const number = 40;
+// const res = number > 15 && number < 35;
+// console.log(res);
+
+
+
+// ||
+// const num = 40;
+// const res = num < 10 || num > 30;
+// console.log(res);
+
+// const number = 20;
+// const result = number < 10 || number > 30;
+// console.log(result);
+
+
+
+// !
+console.log(true);
+console.log(!true);
+console.log(!1);//false
+console.log(!0);//true
+
+// 0 = false; 1 = true
+
+
+
+//if + else
+// let cost = 0
+// const sub = 'pro';
+
+// if (sub === 'premium') {
+//     cost = 100;
+// }
+// console.log(cost);
+
+// let cost;
+// const sub = 'free';
+// if (sub === 'pro') {
+//     cost = 100;
+// }
+// else {
+//     cost = 0;
+// }
+// console.log(cost)
+
+
+// let cost;
+// const sub = 'premium';
+
+// if (sub === 'free') {
+//     cost = 0;
+// }
+// else if (sub === 'pro') {
+
+// }
+
+
+
+// let type;
+// const age = 20;
+
+// if (age >= 18) {
+//     type = 'adult'
+// }
+// else {
+//     type = ' child'
+// }
+// console.log(type);
+
+
+// const age = 20;
+// const type = age >=18 ? 'adult' : 'child';
+// console.log(type);
+
+
+// const num1 = 5;
+// const num2 = 10;
+// let bigger;
+
+// if (num1 > num2) {
+//     bigger = num1;
+// } else {
+//     bigger = num2;
+// }
+// console.log(bigger);
+
+
+// const bigger = num1 > num2 ? num1 : num2;
+// console.log(bigger);
+
+
+
+
+// let question = prompt('Яка “офіційна” назва JavaScript?');
+// if ( question === 'ECMAScript') {
+//     alert('Правильно!')
+// } else {
+//      alert('Ви не знаєте? ECMAScript!')
+// }
+
+
+const a = 1;
+const b = 1;
+const result = a + b < 4 ? 'Нижче':  'Вище';
+console.log(result);
