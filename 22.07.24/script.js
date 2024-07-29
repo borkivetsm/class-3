0a38b3eb7087ee51e7f99d2d1099e155b299dec5
Margarita Borkivets <margo.borkivets@gmail.com>
2024-07-29 13:32:33 +0200

    22.07.24 class

---
+++ 22.07.24/script.js
@@ -0,0 +1,82 @@
+let counter = 0;
+
+while (counter < 10) {
+    console.log('counter: ', counter)
+    counter += 1;
+}
+
+
+
+let clients = 12;
+const maxClients = 20;
+while (clients < maxClients) {
+    console.log(clients);
+    clients += 1;
+}
+
+
+
+// let password = '';
+// do {
+//     password = prompt('Enter a password longer than 4 symbols', '')
+// } while (password.length < 5);
+//     console.log('Your password:', password);
+
+
+
+
+// const max = 15;
+// for (let i = 0; i < max; i += 2) {
+//     console.log(i)
+// } 
+
+
+// const max = 10;
+// for(let i = 0; i < max; i += 1) {
+//     console.log('${max} % 4{i} = ', max % i);
+// }
+
+
+// for (let i = 0; i < 10; i += 1) {
+//     if (i === 5) {
+//         break;
+//     }
+// }
+
+
+
+// const num = 30;
+// for (let i = 0; i < num; i += 1) {
+//     if(i % 2 === 0) {
+//         continue
+//     }
+//     console.log('uneven: ', i)
+// }
+
+
+
+// let num = 1;
+// while (num <= 5) {
+//     console.log(num);
+//     num ++;
+// }
+
+
+// let num = 1;
+// do {
+//     console.log(num);
+//     num ++;
+// } while (num <= 5)
+
+
+// let num = 5;
+// for (let i = 1; i <= num; i += 1) {
+//     console.log(i);
+// }
+
+
+let number = 0;
+while (number < 5) {
+    number ++;
+    console.log(`number: ${number}`);
+}
