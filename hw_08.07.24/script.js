dd833cabdf1c2b0dd558f46d5f5adbdc2f094252
Margarita Borkivets <margo.borkivets@gmail.com>
2024-07-29 14:13:20 +0200

    08.07.24 hw

--- hw_08.07.24/script.js
+++ hw_08.07.24/script.js
@@ -21,11 +21,33 @@ function checkFieldsTwo() {
 }
 
 //3
-function checkFieldJS() {
+function checkFieldThree() {
     const fieldValueText = document.getElementById("text-check").value;
     if (fieldValueText.includes("JavaScript")) {
         alert("Текст містить слово JavaScript");
     } else {
         alert("Текст не містить слово JavaScript");
+    }
+}
+
+//4
+function checkFieldsFour() {
+    const enteredNumber = document.getElementById("entered-number").value;
+    if (enteredNumber > 10 && enteredNumber < 20) {
+        alert("Число входить в діапазон від 10 до 20");
+    } else {
+        alert("Число не входить в діапазон від 10 до 20");
+    }
+}
+
+//5
+function checkFieldFive() {
+    const nameCheck = document.getElementById("name-check").value;
+    const emailCheck = document.getElementById("email-check").value;
+    const passwordCheck = document.getElementById("password-check").value;
+    if (nameCheck.length > 3 || emailCheck.includes("@") || passwordCheck.length > 6) {
+        alert("you're being redirected")
+    } else {
+        alert("there's a mistake")
     }
 }
