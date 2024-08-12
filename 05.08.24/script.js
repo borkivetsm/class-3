5defefc0755541acb2da47b3118a20f0b601b46d
Margarita Borkivets <margo.borkivets@gmail.com>
2024-08-12 17:03:13 +0200

    class 5.08

---
+++ 05.08.24/script.js
@@ -0,0 +1,60 @@
+// //split
+// const message = 'Welcome to Ukraine';
+// console.log(message.split(' '));
+// console.log(message.split(''));
+
+
+// //join
+// const names = ['Bob', 'Alex', 'Bill', 'John'];
+// console.log(names.join(' '));
+// console.log(names.join('*'));
+
+
+// //indexOf
+// const namesNew = ['Bob', 'Alex', 'Bill', 'John'];
+// console.log(namesNew.indexOf('John'));
+// console.log(namesNew.indexOf('bob'));
+
+
+// //includes
+// const newNames = ['Bob', 'Alex', 'Bill', 'John'];
+// console.log(newNames.includes('Alex'));
+
+// const fruit = 'apple'
+// if (fruit === 'apple' || fruit === 'cherry') {
+//     console.log('red')
+// }
+
+// const red = ['apple', 'cherry', 'strawberry'];
+// const fruit = 'cherry';
+// if (red.includes(fruit)) {
+//     console.log('red')
+// }
+
+
+// //push & pop
+
+// const arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(5)
+// console.log(arr);
+// arr.pop();
+// arr.pop();
+// console.log(arr);
+
+
+// //shift
+// const names = ['Bob', 'Alex', 'Bill', 'John'];
+// console.log(names.shift());
+// console.log(names.shift());
+// console.log(names);
+
+// console.log(names.unshift('Nick'));
+// console.log(names);
+
+// //slice
+// const names = ['Bob', 'Alex', 'Bill', 'John'];
+// console.log(names.slice(1, 3));
+
+
