250e52b2c3904c18124a5619e57b2acafa3065c9
Margarita Borkivets <margo.borkivets@gmail.com>
2024-10-10 17:00:20 +0200

    hw 16

--- hw_16/script.js
+++ hw_16/script.js
@@ -1,9 +1,33 @@
 //1
+class Account {
+    constructor({ email, login }) {
+        this.login = login;
+        this.email = email;
+    }
+    getInfo() {
+        return `Login: ${this.login}, Email: ${this.email}`;
+    }
+}
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+ login: 'Mangozedog',
+ email: 'mango@dog.woof',
+});
+
+console.log(mango.getInfo()); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+ login: 'Poly',
+ email: 'poly@mail.com',
+});
+
+console.log(poly.getInfo()); // Login: Poly, Email: poly@mail.com
 
 
 //2
 class User {
-    constructor(name, age, followers) {
+    constructor({ name, age, followers }) {
         this.name = name;
         this.age = age;
         this.followers = followers;
@@ -14,22 +38,162 @@ class User {
     }
 }
 
-const mango = new User({
-    name: 'Mango',
+const margo = new User({
+    name: 'Margo',
     age: 2,
     followers: 20,
    });
    
-   console.log(mango.getInfo()); // User Mango is 2 years old and has 20 followers
+console.log(margo.getInfo()); // User Mango is 2 years old and has 20 followers
    
-   const poly = new User({
-    name: 'Poly',
-    age: 3,
-    followers: 17,
-   });
-   
-   console.log(poly.getInfo()); // User Poly is 3 years old and has 17 followers
+const polly = new User({
+name: 'Polly',
+age: 3,
+followers: 17,
+});
+
+console.log(polly.getInfo()); // User Poly is 3 years old and has 17 followers
 
 
-   //3
+//3
+class Storage {
+constructor(items) {
+    this.items = items
+}
+getItem() {
+    return this.items
+}
+addItem(item) {
+    return this.items.push(item)
+}
+removeItem(item) {
+    const itemIndex = this.items.indexOf(item)
+    if (itemIndex !== -1) {
+        this.items.splice(itemIndex, 1);
+    }
+}
+}
+const storage = new Storage([
+'Нанітоіди',
+'Пролонгер',
+'Залізні жупи',
+'Антигравітатор',
+]);
+
+const items = storage.getItem();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem('Дроїд');
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+
+//4
+class StringBuilder {
+    constructor(str) {
+        this._value = str;
+    }
+    get value() {
+        return this._value;
+    }
+    append(str) {
+        this._value += str
+    }
+    prepend(str) {
+        this._value= str + this.value;
+    }
+    pad(str) {
+        this._value = str + this._value + str
+    }
+}
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+
+
+//5
+class Car {
+    /*
+     * Додай статичний метод `getSpecs(car)`,
+     * який приймає об'єкт-машину як параметр і виводить
+     * в консоль значення властивостей maxSpeed, speed, isOn, distance и price.
+     */
    
+    /*
+     * Конструктор отримує об'єкт налаштувань.
+     *
+     * Додай властивості майбутнього екземпляра класу:
+     * speed - поточна швидкість, початкова 0
+     * price - ціна автомобіля
+     * maxSpeed - максимальна швидкість
+     * isOn - заведений автомобіль, значення true або false. Спочатку false
+     * distance - загальний кілометраж, спочатку 0
+     */
+    constructor() {}
+   
+    /*
+     * Додай геттер і сеттер для властивості price,
+     * який буде працювати з властивістю ціни автомобіля.
+     */
+   
+    /*
+     * Додай код для того, щоб завести автомобіль
+     * Записує у властивість isOn значення true
+     */
+    turnOn() {}
+   
+    /*
+     * Додай код для того, щоб заглушити автомобіль
+     * Записує у властивість isOn значення false,
+     * і скидає поточну швидкість в 0
+     */
+    turnOff() {}
+   
+    /*
+     * Додає до властивості speed отримане значення,
+     * за умови, що результуюча швидкість
+     * не більше, ніж значення властивості maxSpeed
+     */
+    accelerate(value) {}
+   
+    /*
+     * Забирає від властивості speed отримане значення,
+     * за умови, що результуюча швидкість не менше нуля
+     */
+    decelerate(value) {}
+   
+    /*
+     * Додає в поле distance кілометраж (hours * speed),
+     * але тільки в тому випадку, якщо машина заведена!
+     */
+    drive(hours) {}
+   }
+   
+   const mustang = new Car({ maxSpeed: 200, price: 2000 });
+   
+   mustang.turnOn();
+   mustang.accelerate(50);
+   mustang.drive(2);
+   
+   Car.getSpecs(mustang);
+   // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+   
+   mustang.decelerate(20);
+   mustang.drive(1);
+   mustang.turnOff();
+   
+   Car.getSpecs(mustang);
+   // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+   
+   console.log(mustang.price); // 2000
+   mustang.price = 4000;
+   console.log(mustang.price); // 4000
