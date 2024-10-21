d8e1d5ab5ab81fcfcd8736341753d1694f07ad2f
Margarita Borkivets <margo.borkivets@gmail.com>
2024-10-21 19:22:11 +0200

    18

---
+++ 18/script.js
@@ -0,0 +1,50 @@
+// const title = document.createElement('h1');
+// console.log(title);
+
+// title.textContent = 'Hello!';
+// console.log(title);
+
+// const image = document.createElement('img');
+// image.src = 'https://images.app.goo.gl/6vZ7wsfCE3DCSFFS6';
+// image.alt = 'star';
+// console.log(image);
+
+// const text = document.createElement('p');
+// text.textContent = 'GoIteens';
+
+// document.body.appendChild(title);
+// document.body.appendChild(image);
+// document.body.append(text);
+// document.body.prepend(text);
+// document.body.before(text);
+
+// image.remove();
+
+// const header = '<h1>Hello!</h1>';
+// const image = '<img src="https://images.app.goo.gl/6vZ7wsfCE3DCSFFS6", alt = "Star">'
+
+// const contentDiv = document.getElementById('content');
+// contentDiv.innerHTML = header + image;
+
+// const saveBtn = document.querySelector('button[data-action="save"]');
+// console.log(saveBtn.dataset.action);
+
+// const closeBtn = document.querySelector('button[data-action="close"]');
+// console.log(closeBtn.dataset.action);
+
+
+// const img = document.createElement('img');
+// img.src = 'https://images.app.goo.gl/6NhBD437tHL1qTDh8';
+// img.alt = 'cat';
+// img.style.width = '200px';
+// img.style.height = '200px';
+
+// const btn = document.createElement('button');
+// button.type = 'button';
+
+// document.body.appendChild(img);
+// document.body.appendChild(btn);
+
+// const title = document.createElement('h1');
+// title.textContent = "Hello, it's my first created element!";
+// title.classList.add
